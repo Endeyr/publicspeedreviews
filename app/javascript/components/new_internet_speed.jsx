2e3b623731a729164eee5f03f99213fbbc9391c6
Endeyr <aaron.thompson42594@gmail.com>
2023-01-13 08:43:35 -0500

    added style form to collect data

--- app/javascript/components/new_internet_speed.jsx
+++ app/javascript/components/new_internet_speed.jsx
@@ -12,6 +12,42 @@ export default function NewInternetSpeed() {
 					</h2>
 				</div>
 			</div>
+			<div className="md:ml-2 mt-2 w-96">
+				<label
+					className="block mb-2 text-sm font-bold text-gray-700"
+					htmlFor="Place"
+				>
+					Place Name
+				</label>
+				<input
+					className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+					id="placeName"
+					type="text"
+					placeholder="Place Name"
+				/>
+			</div>
+			<div className="md:ml-2 mt-2 w-96">
+				<label
+					className="block mb-2 text-sm font-bold text-gray-700"
+					htmlFor="city"
+				>
+					City
+				</label>
+				<input
+					className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+					id="city"
+					type="text"
+					placeholder="City"
+				/>
+			</div>
+			<div className="md:ml-2 text-center mt-6 w-96">
+				<button
+					className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+					type="button"
+				>
+					Start Speed Test
+				</button>
+			</div>
 		</div>
 	)
 }
